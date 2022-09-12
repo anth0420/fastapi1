56b2e84b0875fa6645b2d00e849864d240e824db
anthony belen <anthonybelenbatista@gmail.com>
2022-09-12 13:17:07 -0400

    añadiendo alertas

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
-from app.routers import user, auth
+from app.routers import user, auth , web
 from app.db.database import Base,engine
 
 
 
 app = FastAPI()
 app.include_router(user.router)
-app.include_router(auth.router)
 
+app.include_router(auth.router)
+app.include_router(web.router)
 
 if __name__=="__main__":
     uvicorn.run("main:app", port=8000,reload=True)
